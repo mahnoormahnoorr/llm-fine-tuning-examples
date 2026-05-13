8e4ec12688875fdb21f9b9541d1b6eabd49bff6c
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2026-05-13 13:47:56 +0300

    updating to latest container

--- run-finetuning-lumi-gpu8-accelerate.sh
+++ run-finetuning-lumi-gpu8-accelerate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=project_462000007
+#SBATCH --account=project_xxxxxxxxx
 #SBATCH --partition=dev-g
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=56
@@ -8,8 +8,10 @@
 #SBATCH --gpus-per-node=8
 
 module purge
-module use /appl/local/csc/modulefiles/
-module load pytorch/2.5
+module use /appl/local/laifs/modules
+module load lumi-aif-singularity-bindings
+
+export SIF="/appl/local/laifs/containers/lumi-multitorch-u24r70f21m50t210-20260415_130625/lumi-multitorch-full-u24r70f21m50t210-20260415_130625.sif"
 
 # This will store all the Hugging Face cache such as downloaded models
 # and datasets in the project's scratch folder
@@ -34,11 +36,13 @@ shift  # remove first argument from argument list
 
 set -xv  # print the command so that we can verify setting arguments correctly from the logs
 
-srun accelerate launch \
-     --config_file=$ACCELERATE_CONFIG \
-     --num_processes=$SLURM_GPUS_PER_NODE \
-     --num_machines=1 \
-     --machine_rank=0 \
-     finetuning.py $* \
-     --output-path $OUTPUT_DIR \
-     --num-workers 7
+srun singularity exec "$SIF" bash -lc "
+    accelerate launch \
+        --config_file='$ACCELERATE_CONFIG' \
+        --num_processes='$SLURM_GPUS_PER_NODE' \
+        --num_machines=1 \
+        --machine_rank=0 \
+        finetuning.py \"\$@\" \
+        --output-path '$OUTPUT_DIR' \
+        --num-workers 7
+" bash "$@"
