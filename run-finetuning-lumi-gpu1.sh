9d8da94ef020ae6b2b50d30b267ad1e43371e4cb
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2026-05-13 10:13:18 +0300

    updating .sh

--- run-finetuning-lumi-gpu1.sh
+++ run-finetuning-lumi-gpu1.sh
@@ -27,9 +27,7 @@ mkdir -p $OUTPUT_DIR
 # want to retain direct control of parallelism options.
 export TOKENIZERS_PARALLELISM=false
 
-# Disable Weights & Biases logging
-export WANDB_DISABLED=true
-export WANDB_MODE=disabled
+
 
 set -xv  # print the command so that we can verify setting arguments correctly from the logs
 
